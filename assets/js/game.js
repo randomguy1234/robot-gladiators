d96599e7f3d0313f7db3b547360258341adf04d7
Jimmy Rando <jsturat46925@gmail.com>
2021-12-04 17:08:03 -0500

    added a little to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!");
+//the creates a function named "fight"
+
+function fight(){
+    window.alert("The fight has begun!");
+}
+
+var playerName= window.prompt("What is your robot's name?");
+
+//forgot abour this
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+//string concatenation
+console.log("Our robot's name is "+playerName);
+
+
+//fight();
